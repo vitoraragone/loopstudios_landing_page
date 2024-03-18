fb82dc0665eb570745c81be18449b6e435c105ae
Vitor Aragone <aragonegoulart@outlook.com>
2024-03-18 18:58:15 -0300

    first commit

---
+++ mocks/index.js
@@ -0,0 +1,34 @@
+export const CreationsList = [
+  {
+    creation_name: "Deep Earth",
+    image_source: "/images/desktop/image-deep-earth.jpg",
+  },
+  {
+    creation_name: "Night Arcade",
+    image_source: "/images/desktop/image-night-arcade.jpg",
+  },
+  {
+    creation_name: "Soccer Team VR",
+    image_source: "/images/desktop/image-soccer-team.jpg",
+  },
+  {
+    creation_name: "The Grid",
+    image_source: "/images/desktop/image-grid.jpg",
+  },
+  {
+    creation_name: "From Up Above VR",
+    image_source: "/images/desktop/image-from-above.jpg",
+  },
+  {
+    creation_name: "Pocket Borealis",
+    image_source: "/images/desktop/image-pocket-borealis.jpg",
+  },
+  {
+    creation_name: "The Curiosity",
+    image_source: "/images/desktop/image-curiosity.jpg",
+  },
+  {
+    creation_name: "Make It Fisheye",
+    image_source: "/images/desktop/image-fisheye.jpg",
+  },
+];
